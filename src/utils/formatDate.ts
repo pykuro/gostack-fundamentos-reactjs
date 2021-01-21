2e5239ef2949f0ac177356d862c4b6c4404352e1
Paula <paula.kuromoto@gmail.com>
2021-01-21 11:32:44 -0300

    conclusao do desafio

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,7 @@
+const formatDate = (value: Date): string =>
+  new Intl.DateTimeFormat('pt-BR', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).format(value);
+export default formatDate;
